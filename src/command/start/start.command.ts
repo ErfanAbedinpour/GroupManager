b06682b0850c557e8ac12377617f24867ee0065e
erfan <erfan.wtf44@gmail.com>
2025-03-11 09:25:37 -0400

    chore: Adding BotMessages

--- src/command/start/start.command.ts
+++ src/command/start/start.command.ts
@@ -1,9 +1,10 @@
 import { CommandContext, Context } from "grammy";
 import { BotCommand } from "../abstract/command.abstract";
+import { BotMessages } from "../../messages/bot.messages.enum";
 
 export class StartCommand implements BotCommand {
 
     async doProcess(ctx: CommandContext<Context>): Promise<void> {
-        ctx.reply("hello")
+        ctx.reply(BotMessages.welcomeMessage)
     }
 }
