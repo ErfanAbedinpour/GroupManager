44a5d4f3e10082d474b24ffcd0acf91e8bd53fde
erfan <erfan.wtf44@gmail.com>
2025-03-12 07:32:32 -0400

    Chroe: Adding Authorization Middleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,34 @@
+import { Context, NextFunction } from "grammy";
+import { Permission } from "../enums/permission.enum";
+
+export function AuthorizationMiddleware(permissions?: Permission[]) {
+
+    return async function (ctx: Context, next: NextFunction): Promise<void> {
+        const fromId = ctx.from?.id;
+
+        if (!fromId)
+            return
+
+        try {
+            const user = (await ctx.getChatAdministrators()).find((admin) => admin.user.id === fromId);
+
+            if (!user)
+                throw new Error("فقط ادمین میتواند از ایم کامند استفاده کند")
+
+            if (!permissions)
+                return await next()
+
+            for (const permission of permissions) {
+                if (!user[permission] && user['status'] === 'administrator') {
+                    throw new Error("شما دسترسی استفاده از این کامند را ندارید")
+                }
+            }
+
+            return await next()
+
+        } catch (err) {
+            ctx.reply(err.message, { reply_markup: { force_reply: true } })
+        }
+
+    }
+}
--- src/middleware/isMember.middleware.ts
+++ src/middleware/isMember.middleware.ts
@@ -1,7 +1,6 @@
 import { Context, NextFunction } from "grammy";
 
 export async function isMemberMiddleware(ctx: Context, next: NextFunction): Promise<void> {
-    console.log("i am here")
     if ((ctx.from && !ctx.from.is_bot))
         return await next()
 
