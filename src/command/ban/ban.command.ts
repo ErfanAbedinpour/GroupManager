9c9cc49e229cfbda949cd803b6a16895a6ad4048
erfan <erfan.wtf44@gmail.com>
2025-03-12 05:23:12 -0400

    fix: Bind Actions To Own Classes

--- src/command/ban/ban.command.ts
+++ src/command/ban/ban.command.ts
@@ -3,7 +3,35 @@ import { BotCommand } from "../abstract/command.abstract";
 
 export class BanCommand implements BotCommand {
     async doProcess(ctx: CommandContext<Context>): Promise<void> {
-        console.log(ctx.message)
-        return
+        const userMustBeBan = ctx.message?.reply_to_message?.from?.id;
+        const length = ctx.message!.entities[0].length
+        const date = Number(ctx.message!.text.slice(length));
+
+        try {
+
+            if (!userMustBeBan)
+                throw new Error("please reply to target User!")
+
+            if (!date)
+                throw new Error("invalid date")
+
+            const until = date * 24 * 60 * 60 * 1000 + Date.now();
+
+
+            const result = await this.banChatMember(ctx, until, userMustBeBan)
+
+            if (!result)
+                throw new Error("error During Ban User")
+
+            return
+        } catch (err) {
+            ctx.reply(err.message)
+        }
     }
+
+
+    private banChatMember(ctx: CommandContext<Context>, until: number, user_id: number) {
+        return ctx.banChatMember(user_id, { revoke_messages: true, until_date: until })
+    }
+
 }
