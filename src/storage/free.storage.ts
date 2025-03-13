a6cf650b4339a3d651ada206de4ea049322b6ed3
erfan <erfan.wtf44@gmail.com>
2025-03-13 14:52:14 -0400

    chore: Adding Session Storage

--- src/storage/free.storage.ts
+++
@@ -1,5 +0,0 @@
-import { freeStorage } from "@grammyjs/storage-free";
-import { SessionData } from "../types/session.interface";
-import { Config } from "../config/global.config";
-
-export const sessionStorage = freeStorage<SessionData>(Config.BotToken);
